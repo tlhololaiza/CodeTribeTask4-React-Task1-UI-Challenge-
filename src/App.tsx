4f9d7c98f5ac21a55a0e1114c25f7489e49ba50d
tlhololaiza <tshwanetlholo7@gmail.com>
2025-07-31 22:18:16 +0200

    Update 2

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,13 @@ import Footer from './components/Footer/Footer'
 import Hero from './components/Hero/Hero'
 import Text from './components/Text/Text'
 import Category from './components/Category/Category'
+import pic_5 from './assets/images/Custom Cake.jpeg';
+import pic_6 from '/src/assets/images/Macaron.jpeg';
+import pic_7 from '/src/assets/images/Cupcake Collection.jpeg';
+import pic_8 from '/src/assets/images/Holiday.jpeg';
+import pic_9 from '/src/assets/images/No title_no desc.jpeg';
+import pic_10 from '/src/assets/images/No title_no desc.jpeg';
+
 
 function App() {
   
@@ -15,18 +22,18 @@ function App() {
       <Text desc = "Delight in every Bite!" font="'Dancing Script', cursive" color="black" size="3rem" />
 
       <div className='categories'>
-        <Category title="Custom Cakes" image="/src/assets/images/Custom Cake.jpeg" desc="Handmade and delicious cakes for any occasion." />
-        <Category title="Macarons" image="/src/assets/images/Macaron.jpeg" desc="Colorful, delicate, and sweet macarons you'll love." />
-        <Category title="Cupcakes" image="/src/assets/images/Cupcake Collection.jpeg" desc="Perfect cupcakes topped with creamy frosting." />
+        <Category title="Custom Cakes" image={pic_5} desc="Handmade and delicious cakes for any occasion." />
+        <Category title="Macarons" image={pic_6} desc="Colorful, delicate, and sweet macarons you'll love." />
+        <Category title="Cupcakes" image={pic_7} desc="Perfect cupcakes topped with creamy frosting." />
         
       </div>
       
       <Text desc = "~Cupcake Collection!~" font="'Dancing Script', cursive" color="gold" size="3rem" />
 
       <div className='categories'>
-        <Category title="-20% OFF" image="/src/assets/images/Holiday.jpeg" desc="" />
-        <Category title="" image="/src/assets/images/No title_no desc.jpeg" desc="" />
-        <Category title="Holiday Collection" image="/src/assets/images/Holiday Collection.jpeg" desc="Cakes for the holiday" />
+        <Category title="-20% OFF" image={pic_8} desc="" />
+        <Category title="" image={pic_9} desc="" />
+        <Category title="Holiday Collection" image={pic_10} desc="Cakes for the holiday" />
         
       </div>
       
