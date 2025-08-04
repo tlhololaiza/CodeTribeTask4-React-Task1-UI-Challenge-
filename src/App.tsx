611e28d5ed8b10d19a9e3218e4645063a514964b
tlhololaiza <tshwanetlholo7@gmail.com>
2025-08-05 01:28:31 +0200

    edited one image

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import Footer from './components/Footer/Footer'
 import Hero from './components/Hero/Hero'
 import Text from './components/Text/Text'
 import Category from './components/Category/Category'
-import pic_5 from './assets/images/Custom Cake.jpeg';
+import picture_5 from './assets/images/Custom Cake.jpeg';
 import pic_6 from '/src/assets/images/Macaron.jpeg';
 import pic_7 from '/src/assets/images/Cupcake Collection.jpeg';
 import pic_8 from '/src/assets/images/Holiday.jpeg';
 import pic_9 from '/src/assets/images/No title_no desc.jpeg';
-import pic_10 from '/src/assets/images/No title_no desc.jpeg';
+import pic_10 from '/src/assets/images/Holiday Collection.jpeg';
 
 
 function App() {
@@ -22,7 +22,7 @@ function App() {
       <Text desc = "Delight in every Bite!" font="'Dancing Script', cursive" color="black" size="3rem" />
 
       <div className='categories'>
-        <Category title="Custom Cakes" image={pic_5} desc="Handmade and delicious cakes for any occasion." />
+        <Category title="Custom Cakes" image={picture_5} desc="Handmade and delicious cakes for any occasion." />
         <Category title="Macarons" image={pic_6} desc="Colorful, delicate, and sweet macarons you'll love." />
         <Category title="Cupcakes" image={pic_7} desc="Perfect cupcakes topped with creamy frosting." />
         
