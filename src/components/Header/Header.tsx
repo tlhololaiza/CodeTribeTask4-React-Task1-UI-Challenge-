bd39a4e44587052de69fbcd73aae166e09e77807
tlhololaiza <tshwanetlholo7@gmail.com>
2026-01-12 20:27:51 +0200

    Updated the header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,19 +2,21 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <nav>
-      
-        <h2>Patisserie</h2>
-        <div className='links'>
-          <a href="/#" className='link'>Home</a>
-          <a href="/#" className='link'>Menu</a>
-          <a href="/#" className='link'>Cakes</a>
-          <a href="/#" className='link'>Parties</a>
-          <a href="/#" className='link'>Contacts</a>
-          <button className='oder-btn'>ORDER NOW</button>
-          
-        </div>
-      
+    <nav className="nav">
+      <div className="brand">Patisserie</div>
+
+      <div className="links">
+        <a href="/#" className="link">HOME</a>
+        <a href="/#" className="link">BIEN</a>
+        <a href="/#" className="link">CAKES</a>
+        <a href="/#" className="link">PARTIES</a>
+        <a href="/#" className="link">CONTACT</a>
+      </div>
+
+      <div className="actions">
+        <span className="cart" aria-label="Cart" role="img">🛒</span>
+        <button className="order-btn">ORDER ONLINE NOW</button>
+      </div>
     </nav>
   )
 }
