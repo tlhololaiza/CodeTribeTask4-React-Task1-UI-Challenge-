321d8071b9ea65f5ae3c1e7b104a8d7f7fe21a12
tlhololaiza <tshwanetlholo7@gmail.com>
2026-01-12 23:34:13 +0200

    changed the start icon

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,9 +1,10 @@
 import './Hero.css';
+import starIcon from '../../assets/icons/star.png'
 
 const Hero = () => {
   return (
     <section className="hero">
-      <div className="hero-logo">🏵️</div>
+      <img src={starIcon} alt="Patisserie" className="hero-logo" />
       <h1>Delight in</h1>
       <p className="hero-sub">PATISSERIE IN EVERY BITE!</p>
       <button className="hero-btn">ORDER NOW</button>
